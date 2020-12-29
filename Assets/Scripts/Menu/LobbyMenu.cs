ae2cdd6a6341e65295ea9a860cdbc46a752880a1
mattistern <mattistern1@gmail.com>
2020-12-30 00:13:31 +0200

    start game
    button connect

--- Assets/Scripts/Menu/LobbyMenu.cs
+++ Assets/Scripts/Menu/LobbyMenu.cs
@@ -3,24 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LobbyMenu : MonoBehaviour
 {
     [SerializeField] private GameObject lobbyUI = null;
+    [SerializeField] private Button startGameButton = null; 
 
     private void Start()
     {
         WaterFieldNetworkManager.ClientOnConnected += HandleClientConnected;
+        WaterFieldPlayer.AuthOnLobbyOwnerUpdated += AuthHandleOnLobbyOwnerUpdated;
     }
 
     private void OnDestroy()
     {
         WaterFieldNetworkManager.ClientOnConnected -= HandleClientConnected;
+        WaterFieldPlayer.AuthOnLobbyOwnerUpdated -= AuthHandleOnLobbyOwnerUpdated;
+
     }
 
     private void HandleClientConnected()
     {
         lobbyUI.SetActive(true);
+    }
+
+    private void AuthHandleOnLobbyOwnerUpdated(bool state)
+    {
+        startGameButton.gameObject.SetActive(state);
+    }
+
+    public void StartGame()
+    {
+        NetworkClient.connection.identity.GetComponent<WaterFieldPlayer>().CmdStartGame();  
     }
 
     public void LeaveLobby()
--- Assets/Scripts/Network/WaterFieldNetworkManager.cs
+++ Assets/Scripts/Network/WaterFieldNetworkManager.cs
@@ -9,8 +9,69 @@ public class WaterFieldNetworkManager : NetworkManager
     public static event Action ClientOnConnected;
     public static event Action ClientOnDisconnected;
 
-    //public List
+    private bool isGameInProgress = false;
 
+    public List<WaterFieldPlayer> Players { get; } = new List<WaterFieldPlayer>();
+
+    #region Server
+
+    public override void OnServerConnect(NetworkConnection conn)
+    {
+        if (!isGameInProgress) { return; }
+
+        conn.Disconnect();
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        WaterFieldPlayer player = conn.identity.GetComponent<WaterFieldPlayer>();
+        Players.Add(player);
+
+        player.SetPartyOwner(Players.Count == 1);
+    }
+
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        WaterFieldPlayer player = conn.identity.GetComponent<WaterFieldPlayer>();
+        Players.Remove(player);
+
+        base.OnServerDisconnect(conn);
+    }
+
+    public override void OnStopServer()
+    {
+        Players.Clear();
+        isGameInProgress = false;
+    }
+
+    public void StartGame()
+    {
+        if(Players.Count < 1) { return; }
+
+        isGameInProgress = true;
+        Debug.Log("started");
+
+        ServerChangeScene("SampleScene");
+    }
+
+
+
+    public override void OnServerChangeScene(string newSceneName)
+    {
+        base.OnServerChangeScene(newSceneName);
+        //TODO own code
+        /*foreach (WaterFieldPlayer player in Players)
+        {
+            GameObject playerInstance = Instantiate(//prefab, GetStartPosition().position, Quaternion.identity);
+            NetworkServer.Spawn(playerInstance, player.connectionToClient);
+        }*/
+    }
+
+    #endregion
+
+    #region Client
     public override void OnClientConnect(NetworkConnection conn)
     {
         base.OnClientConnect(conn);
@@ -23,5 +84,14 @@ public class WaterFieldNetworkManager : NetworkManager
 
         ClientOnDisconnected?.Invoke();
     }
+
+    public override void OnStopClient()
+    {
+        Players.Clear();
+    }
+
+    #endregion
+
+
 
 }
