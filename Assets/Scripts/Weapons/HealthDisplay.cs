b5e446444fc014556dd3cfd01db581be49fff844
mattistern <mattistern1@gmail.com>
2021-01-08 14:08:54 +0200

    healthBar

---
+++ Assets/Scripts/Weapons/HealthDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [SerializeField] private Health health = null;
+    [SerializeField] private Image healthBar = null;
+
+    private void Awake()
+    {
+        health.ClientOnHealthUpdated += ImageUpdateUI;
+    }
+
+    private void OnDestroy()
+    {
+        health.ClientOnHealthUpdated -= ImageUpdateUI;
+    }
+
+    private void ImageUpdateUI(int currentHealth, int maxHealth)
+    {
+        healthBar.fillAmount = (float)currentHealth / maxHealth;
+    }
+}
